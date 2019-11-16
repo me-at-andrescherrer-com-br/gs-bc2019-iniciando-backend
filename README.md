2b684210b4d228ed39f3681f1167f4d8d2cfb497
André Scherrer <andre@computerscience.com.br>
2019-11-16 12:21:30 -0300

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Go Stack 2019 - Iniciando Backend
+# Go Stack 2019 - Inciando o Backend da Aplicação
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '34e1fb29fafa6a6420ff0d7c943f53dd',
+  expiresIn: '7d',
+};
